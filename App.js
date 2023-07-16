41ad262fdcfcaae03a64aeb681f362116bb012fa
Abhigyan Mohanta <110360901+ABHIGYAN-MOHANTA@users.noreply.github.com>
2023-07-16 11:20:22 +0530

    works only when screen on

--- App.js
+++ App.js
@@ -1,11 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Vibration,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  const [intervalValue, setIntervalValue] = useState(5); // Default interval is 5 minutes
+  const [timeLeft, setTimeLeft] = useState(intervalValue * 60); // Time left in seconds
+
+  useEffect(() => {
+    const intervalInMilliseconds = intervalValue * 60 * 1000;
+
+    const vibrateMultipleTimes = (times) => {
+      if (times === 0) {
+        setTimeLeft(intervalValue * 60); // Reset time left after all vibrations
+        return;
+      }
+
+      Vibration.vibrate();
+      setTimeout(() => {
+        vibrateMultipleTimes(times - 1);
+      }, 500); // Delay between vibrations (adjust as needed)
+    };
+
+    const vibrationInterval = setInterval(() => {
+      vibrateMultipleTimes(3); // Trigger multiple vibrations
+    }, intervalInMilliseconds);
+
+    const timer = setInterval(() => {
+      setTimeLeft((prevTimeLeft) => {
+        if (prevTimeLeft === 0) {
+          clearInterval(timer);
+          return 0;
+        }
+        return prevTimeLeft - 1;
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(vibrationInterval);
+      clearInterval(timer);
+    };
+  }, [intervalValue]);
+
+  const handleButtonPress = (newInterval) => {
+    setIntervalValue(newInterval);
+    setTimeLeft(newInterval * 60);
+  };
+
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const formattedMinutes = String(minutes).padStart(2, "0");
+    const formattedSeconds = String(seconds % 60).padStart(2, "0");
+    return `${formattedMinutes}:${formattedSeconds}`;
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>
+        Choose the interval for a time block for your phone to vibrate!
+      </Text>
+      <Text style={styles.intervalText}>{intervalValue} minutes</Text>
+      <Text style={styles.timeLeftText}>
+        Time left for next vibration: {formatTime(timeLeft)}
+      </Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => handleButtonPress(0.1)}
+        >
+          <Text style={styles.buttonText}>0.1 min</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => handleButtonPress(5)}
+        >
+          <Text style={styles.buttonText}>5 min</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => handleButtonPress(10)}
+        >
+          <Text style={styles.buttonText}>10 min</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => handleButtonPress(25)}
+        >
+          <Text style={styles.buttonText}>25 min</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.heading}>
+        After choosing the time interval, put phone in pocket to feel the
+        vibrations!
+      </Text>
     </View>
   );
 }
@@ -13,8 +104,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
+    color: "white",
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 100,
+    marginBottom: 10,
+    color: "white",
+    textAlign: "center",
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  intervalText: {
+    fontSize: 18,
+    marginBottom: 10,
+    color: "white",
+  },
+  timeLeftText: {
+    fontSize: 16,
+    marginBottom: 20,
+    color: "white",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%",
+  },
+  button: {
+    backgroundColor: "#2196F3",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "white",
   },
 });
